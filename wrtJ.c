4f6384fbdedaf48704fb29cf39d8af7fea2df999
Iru2000 <camilagoyechea790@gmail.com>
2025-07-22 12:39:46 -0300

    Eleminando

--- wrtJ.c
+++
@@ -1,9 +0,0 @@
-// wrt_lib.c
-#include "wrt.h"
-
-void wrt_start_from_java(const char *url) {
-    wrt_config_t config;
-    wrt_init_config(&config);
-    strncpy(config.url, url, sizeof(config.url));
-    wrt_start(&config);
-}
